c6d8889f4f44991f1e4b17abb412b2d1c4a495a8
aaryaP-code <aarya19pandit@gmail.com>
2026-09-22 19:09:54 +0530

    Add Encpsulation

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,28 @@
+class Human {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Human obj = new Human();
+        obj.setAge(19);
+        obj.setName("Aarya");
+
+        System.out.println(obj.getName() + " : " + obj.getAge());
+    }
+}
