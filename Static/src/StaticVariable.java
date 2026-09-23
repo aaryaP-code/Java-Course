2b1f8cf610d0ddad02252f9ee89be8e4d79f8fe6
aaryaP-code <aarya19pandit@gmail.com>
2026-09-23 20:40:25 +0530

    Static Variable

---
+++ Static/src/StaticVariable.java
@@ -0,0 +1,29 @@
+class Mobile {
+    String brand;
+    int price;
+    static String name;
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+public class StaticVariable {
+    public static void main(String[] args) {
+
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1_50_000;
+        Mobile.name = "SmartPhone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 2_00_000;
+        Mobile.name = "Fold";
+
+        Mobile.name = "Phone";
+
+        obj1.show();
+        obj2.show();
+    }
+}
